848e1ae9a4b86d0a3ff04093e75932ed060d0ca8
Cpuritan <bqf_cpuritan@163.com>
2026-04-12 15:41:21 +0800

    1

---
+++ src/utils/date.ts
@@ -0,0 +1,8 @@
+const dateFormatter = new Intl.DateTimeFormat("en-CA", {
+  timeZone: "Asia/Shanghai",
+  year: "numeric",
+  month: "2-digit",
+  day: "2-digit"
+});
+
+export const formatDate = (date: Date): string => dateFormatter.format(date);
